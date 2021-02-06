7c736c6a9d88e66209e18620c12d1c803723edd9
Pat <p171982@gmail.com>
2021-02-06 20:55:31 +0000

    soem changes

--- register/process_code/index.php
+++ register/process_code/index.php
@@ -7,10 +7,24 @@ require_once($path."wp-load.php");
 // get submitted values
 if(isset($_POST['register_submit'])){
     // echo print_r($_POST);
-    $email = sanitize_text_field($_POST['user_email']);
+    global $xyz;
+    $xyz = sanitize_text_field($_POST['user_email']);
     $username = sanitize_text_field($_POST['user_username']);
 
-   echo "Thank you for submitting form. You are registered with <br> email: " . $email . " <br> and username: " . $username;
+   
+
+    //insert user data to DB table
+    global $wpdb;
+    // define("USER_EMAIL",$email);
+    $table = 'wp_registered';
+    $data = array('username'=> $username, 'email' => $xyz);
+    $format = array('%s','%s');
+    $success = $wpdb->insert($table,$data,$format);
+    if($success){
+        echo "RECORD WPISANY DO TABELI";
+        // var_dump($email);
+        wp_redirect('http://localhost/exampleTwo/successful-registration/');
+    }
 }
 
 ?>
